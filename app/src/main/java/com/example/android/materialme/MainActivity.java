a2ef5b487cbd421696941d0602d3a63aa36a965f
Pablo <paochoa@unirioja.es>
2021-03-15 10:18:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialme;
+
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/***
+ * Main Activity for the Material Me app, a mock sports news application
+ * with poor design choices.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<Sport> mSportsData;
+    private SportsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerView);
+
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the ArrayList that will contain the data.
+        mSportsData = new ArrayList<>();
+
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new SportsAdapter(this, mSportsData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Get the data.
+        initializeData();
+
+        /*este objeto ayuda para definir que le ocurre a la lista de elementos de un RecyclerView
+        en caso de que tenga lugar una serie de acciones (como swipe,drag, drop,...)
+         */
+
+        /* el cero en el primer parámetro indica las direcciones de movimiento de drag soportadas, y el otro
+        atributo indica las direcciones de swipe soportadas
+         */
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback
+                (ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
+                        ItemTouchHelper.DOWN | ItemTouchHelper.UP,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+
+                //estos son los indices del movimiento del elemento
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+
+                //sirve para intercambiar los elementos del dataset
+                Collections.swap(mSportsData, from, to);
+
+                //sirve para notificar al adaptador que un item ha sido movido
+                mAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+
+                //esto indica que en caso de deslizarse a derecha o izquierda se elminará de la lista
+                mSportsData.remove(viewHolder.getAdapterPosition());
+
+                //esto permite animar la eliminación de un elemento de forma correcta
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+
+        //añade el helper al RecyclerView
+        helper.attachToRecyclerView(mRecyclerView);
+    }
+
+    /**
+     * Initialize the sports data from resources.
+     */
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] sportsList = getResources()
+                .getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources()
+                .getStringArray(R.array.sports_info);
+
+        TypedArray sportsImageResources =
+                getResources().obtainTypedArray(R.array.sports_images);
+
+        // Clear the existing data (to avoid duplication).
+        mSportsData.clear();
+
+        // Create the ArrayList of Sports objects with titles and
+        // information about each sport.
+        for(int i=0;i<sportsList.length;i++){
+            mSportsData.add(new Sport(sportsList[i],sportsInfo[i],sportsImageResources.getResourceId(i,0)));
+        }
+
+        //para limpiar los datos en el array tipado
+        sportsImageResources.recycle();
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void resetSports(View view) {
+        initializeData();
+    }
+}
